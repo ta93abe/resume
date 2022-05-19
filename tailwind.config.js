98f9dc6a56541c3e066eb37d4fe74fb95507f102
ta93abe <ta93abe@ta93abe.com>
2022-05-19 20:59:26 +0900

    headlessui, heroicons and daisyui setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,22 @@ module.exports =
 			"./src/components/**/*.{js,ts,jsx,tsx}",
 		],
 		theme: { extend: { fontFamily: { dotGothic: ["DotGothic16"] } } },
-		plugins: [],
+		daisyui: {
+			themes: [
+				{
+					ta93abe: {
+						"primary": "#9333ea",
+						"secondary": "#c026d3",
+						"accent": "#a3e635",
+						"neutral": "#110E0E",
+						"base-100": "#171212",
+						"info": "#7dd3fc",
+						"success": "#4ade80",
+						"warning": "#fde047",
+						"error": "#e11d48",
+					},
+				},
+			],
+		},
+		plugins: [require("daisyui")],
 	};
