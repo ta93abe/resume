0f7a3eb7abf3562ce242740a4e3be1a2755cf75d
ta93abe <ta93abe@ta93abe.com>
2022-05-19 19:53:03 +0900

    mdx setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const nextConfig = { reactStrictMode: true };
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const withMDX = require("@next/mdx")({
+	extension: /\.mdx?$/,
+	option: { remarkPlugins: [], rehypePlugins: [] },
+});
+
+module.exports =
+	withMDX({ pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"] });
