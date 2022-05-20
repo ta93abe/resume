471c7e5c37a1db7f0f4f01d37a2e8cbf6fe8ef0e
ta93abe <ta93abe@ta93abe.com>
2022-05-20 18:44:21 +0900

    add 500 page

---
+++ src/pages/500.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const InternalServerError: NextPage = () => {
+	return (
+		<>
+      <div className="text-7xl">500</div>
+    </>
+	);
+};
+
+export default InternalServerError;
